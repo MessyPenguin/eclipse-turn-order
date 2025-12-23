83fd26d9e5a7057fd348fc11f0c8d7d6fe5dbe1a
MessyPenguin <giles_chapman@outlook.com>
2025-12-23 17:29:28 +0000

    Update races.js

--- js/races.js
+++ js/races.js
@@ -2,12 +2,27 @@
    RACES + SIMPLE SELECTION LOGIC
 ----------------------------------------------------------- */
 
-const RACES = [
-    { id: "terran",   name: "Terran",   colorClass: "faction-terran" },
-    { id: "eridani",  name: "Eridani",  colorClass: "faction-eridani" },
-    { id: "planta",   name: "Planta",   colorClass: "faction-planta" },
-    { id: "hydran",   name: "Hydran",   colorClass: "faction-hydran" }
+cconst RACES = [
+  // Base Game
+  { id: "terran", name: "Terran Federation", glyph: "assets/glyphs/terran.png" },
+  { id: "planta", name: "Planta", glyph: "assets/glyphs/planta.png" },
+  { id: "hydran", name: "Hydran Progress", glyph: "assets/glyphs/hydran.png" },
+  { id: "orion", name: "Orion Hegemony", glyph: "assets/glyphs/orion.png" },
+  { id: "mechanema", name: "Mechanema", glyph: "assets/glyphs/mechanema.png" },
+  { id: "exiles", name: "Exiles", glyph: "assets/glyphs/exiles.png" },
+  { id: "magellan", name: "Magellan", glyph: "assets/glyphs/magellan.png" },
+  { id: "rhoindi", name: "Rho Indi Syndicate", glyph: "assets/glyphs/rhoindi.png" },
+  { id: "enclave", name: "Enclave of the Void", glyph: "assets/glyphs/enclave.png" },
+  { id: "wardens", name: "Wardens of the Council", glyph: "assets/glyphs/wardens.png" },
+
+  // Expansion Factions
+  { id: "shapers", name: "Shapers of the Void", glyph: "assets/glyphs/shapers.png" },
+  { id: "lyra", name: "Enlightened of Lyra", glyph: "assets/glyphs/lyra.png" },
+  { id: "keepers", name: "Keepers of the Gate", glyph: "assets/glyphs/keepers.png" },
+  { id: "architects", name: "The Architects", glyph: "assets/glyphs/architects.png" },
+  { id: "riftborn", name: "The Riftborn", glyph: "assets/glyphs/riftborn.png" }
 ];
+
 
 function findRaceById(id) {
     return RACES.find(r => r.id === id) || null;
@@ -20,3 +35,4 @@ function getNextRaceId(currentId) {
     if (idx === -1 || idx === RACES.length - 1) return RACES[0].id;
     return RACES[idx + 1].id;
 }
+
